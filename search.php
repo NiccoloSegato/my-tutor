9c537adbfe6f170a798f7f01cfb65175ee3f6508
Niccolò Segato <segato117@gmail.com>
2022-05-16 14:26:32 +0200

    New index completed

---
+++ search.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <title>Cerca un tutor - FuoriKlasse</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="assets/images/cropped-IMG_2385-32x32.jpg" sizes="32x32">
+        <link rel="stylesheet" href="styles/index.css">
+    </head>
+    <body>
+        <header>
+            <img src="assets/images/logo.png" alt="Logo">
+            <p>FuoriKlasse</p>
+        </header>
+        <div id="bodycont">
+            <h1 style="font-weight: 700; font-size: 50px; margin: 0;">Trova il tuo tutor</h1>
+            <p style="margin-top: 10px">Cerca tra tutti i tutor di FuoriKlasse e trova quello che fa per te.</p>
+            
+            <input type="text" placeholder="Inserisci nome, cognome, email o altro...">
+        </div>
+
+        <footer class='footer'>
+            <div id='logo-cont'>
+                <img src="assets/images/logo.png" width="45" height="45" alt="FuoriKlasse logo"></img>
+                <div>
+                    <h4>FuoriKlasse</h4>
+                    <p>La rivoluzione dello studio</p>
+                </div>
+            </div>
+            <a href=''>Chi siamo</a>
+            <a href=''>Inizia ora</a>
+    
+            <p style="font-size: 14px; margin-top: 20px;">Copyright 2022 © FuoriKlasse - Tutti i diritti riservati</p>
+        </footer>
+    </body>
+</html>
