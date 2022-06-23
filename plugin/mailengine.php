7b4f9b29de93f923b150e2770b517aaacf21c263
Niccolò Segato <segato117@gmail.com>
2022-06-23 22:15:01 +0200

    Nuova mail di conferma anche per lil tutor

--- plugin/mailengine.php
+++ plugin/mailengine.php
@@ -22,7 +22,7 @@ if(isset($_GET["dest"]) && isset($_GET["token"]) && isset($_GET["lesson"])){
             die;
         }
 
-        $queryl = $conn->prepare("SELECT tutor.name as tutorname, tutor.surname, subject.name as subjectname, lesson.datereference, lesson.duration FROM lesson, tutor, subject WHERE lesson.id = ? AND subject.tutor = tutor.id AND lesson.subject = subject.id");
+        $queryl = $conn->prepare("SELECT tutor.email as tutormail, tutor.name as tutorname, tutor.surname, subject.name as subjectname, lesson.datereference, lesson.duration FROM lesson, tutor, subject WHERE lesson.id = ? AND subject.tutor = tutor.id AND lesson.subject = subject.id");
         $queryl->bind_param('i', $lesson);
         $queryl->execute();
         $resultl = $queryl->get_result();
@@ -38,6 +38,7 @@ if(isset($_GET["dest"]) && isset($_GET["token"]) && isset($_GET["lesson"])){
                 $subjectname = $rowl["subjectname"];
                 $startingdate = $rowl["datereference"];
                 $duration = $rowl["duration"];
+                $tutormail = $rowl["tutormail"];
             }
         }
 
@@ -100,13 +101,67 @@ if(isset($_GET["dest"]) && isset($_GET["token"]) && isset($_GET["lesson"])){
                             </div>
                             <p>Abbiamo ricevuto il tuo pagamento, il tutor ti contatterà per concordare le modalità e le informazioni utili per la lezione.</p>
                             <div class="divider"></div>
-                            <p>Grazie mille e buona lezione con TutorMate!</p>
+                            <p style="margin-bottom: 30px;">Grazie mille e buona lezione con TutorMate!</p>
                         </body>
                     </html>
                     ', 'text/html');
 
+        $messagetutor = (new Swift_Message('Nuova prenotazione da ' . $dest))
+        ->setFrom(['info@TutorMate.it' => 'TutorMate Team'])
+        ->setTo([$dest])
+        ->setBody('
+        <html lang="it">
+        <html>
+            <head>
+                <title>Nuova prenotazione da ' . $dest . '</title>
+                <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Nunito" />
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <style>
+                    body {
+                        text-align: center;
+                        font-family: Nunito, Helvetica, sans-serif;
+                        width: 90%;
+                        margin: 30px auto;
+                        background-color: #f9f9f9;
+                    }
+                    .divider {
+                        background-color: rgb(220, 220, 220);
+                        height: 2px;
+                        width: 100%;
+                        margin: 10px 0;
+                    }
+                    #res-card {
+                        padding: 10px;
+                        width: calc(100% - 20px);
+                        border-radius: 10px;
+                        background-color: white;
+                    }
+                    #res-card p, #res-card h2 {
+                        margin: 5px;
+                    }
+                </style>
+            </head>
+            <body>
+                <img src="https://tutormate.it/assets/images/logo.png" alt="TutorMate Logo" width="50" height="50">
+                <h1>🚀 Lezione prenotata!</h1>
+                <p>Una tua lezione è stata prenotata su <strong>TutorMate</strong> 🥳</p>
+                <p>Ecco un riepilogo della prenotazione che è stata effettuata</p>
+                <div id="res-card">
+                    <h2>' . $subjectname . '</h2>
+                    <p>Inizierà il <strong>' . $startingdate . '</strong></p>
+                    <p>Durata: <strong>' . $duration . ' minuti</strong></p>
+                </div>
+                <p>Vai alla tua <a href="https://tutormate.it/dashboard/">dashboard</a> per visualizzare il numero di telefono dello studente e i dettagli da lui inseriti</p>
+                <p>Abbiamo ricevuto il pagamento, lo puoi visualizzare nella tua dashboard e sarà disponibile per il prelievo una volta completata la lezione.</p>
+                <div class="divider"></div>
+                <p style="margin-bottom: 30px;">Grazie mille e buona lezione con TutorMate!</p>
+            </body>
+        </html>
+        ', 'text/html');
+
         // Send the message
-        if($mailer->send($message)) {
+        if($mailer->send($message) && $mailer->send($messagetutor)) {
             echo json_encode(['status' => '1', 'dest' => $dest]);
             die;
         }
